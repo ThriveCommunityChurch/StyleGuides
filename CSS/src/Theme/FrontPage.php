efaea41fe66ca85bc18beade8b83a91ae13c7569
Wyatt Baggett <wyattbaggett@yahoo.com>
2017-07-20 10:31:01 -0400

    removed duplicate file

--- CSS/src/Theme/FrontPage.php
+++
@@ -1,3 +0,0 @@
-
-//Front Page ----- poster can be .gif OR png file
-echo do_shortcode("[vidbg container='#front-page-1' mp4='http://thrive-fl.org/wp-content/uploads/2017/07/Home_Video1.mp4' poster='http://thrive-fl.org/wp-content/uploads/2017/07/HomePNG.png' loop='true' overlay='false' overlay_color='#000' overlay_alpha='0.3' muted='true']");
